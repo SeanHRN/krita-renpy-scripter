3866aa0d8c534a32d4901d84bb6021d0b29938b8
Sean Castillo <tealseaweed@gmail.com>
2022-02-16 22:47:25 -0800

    Widget system rework!

--- generateRenpyScripting.py
+++ generateRenpyScripting.py
@@ -6,7 +6,10 @@ from PyQt5.QtWidgets import *
 KI = Krita.instance()
 default_outfile_name = "rpblock.txt"
 indent = 4
+align_values_p1 = [0.0, 0.5, 1.0]
+align_values_p2 = [0.0, 0.25, 0.5, 0.75, 1.0]
 align_values_p3 = [0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0]
+align_values = [align_values_p1, align_values_p2, align_values_p3]
 
 def parseValuesIntoList(name, sub_to_check):
     list = []
@@ -49,8 +52,8 @@ def closestNum(num_list, value):
 #
 def calculateAlign(data_list, precision_level):
     """
-    calculateAlign converts the pos(x,y) coordinates in the data list into
-    align(x,y) coordinates.
+    calculateAlign converts the pos(x,y) coordinates
+    in the data list into align(x,y) coordinates.
     """
     width, height = 1, 1
     currentDoc = KI.activeDocument()
@@ -60,8 +63,8 @@ def calculateAlign(data_list, precision_level):
 
     new_data_list = []
     for d in data_list:
-        xalign = closestNum(align_values_p3, (d[1] / width))
-        yalign = closestNum(align_values_p3, (d[2] / height))
+        xalign = closestNum(align_values[precision_level-1], (d[1] / width))
+        yalign = closestNum(align_values[precision_level-1], (d[2] / height))
         new_data_list.append(tuple((d[0],xalign,yalign)))
     return new_data_list
 
@@ -91,7 +94,6 @@ def getData(button_num, precision_level):
             data_list.append(tuple((name, coord_indv[0], coord_indv[1])))
 
     if button_num == 2:
-        # fill in case for align mode
         data_list = calculateAlign(data_list, precision_level)
     return file_open, data_list
 
@@ -121,39 +123,48 @@ class GenerateRenpyScripting(DockWidget):
         self.createInterface()
 
     def createInterface(self):
-        mainWidget = QWidget(self)
 
-        pos_button = QPushButton("Generate pos(x,y) Scripting", mainWidget)
+
+        pos_button = QPushButton("Generate pos(x,y) Scripting")
         pos_button.clicked.connect(lambda: self.process(1))
 
-        align_button = QPushButton("Generate align(x,y) Scripting", mainWidget)
+        align_button = QPushButton("Generate align(x,y) Scripting")
         align_button.clicked.connect(lambda: self.process(2))
-
-        self.filename_line = QLineEdit()
-        self.filename_line.setText(default_outfile_name)
 
 
         self.precision_slider = QSlider(Qt.Horizontal, self)
         self.precision_slider.setGeometry(30, 40, 200, 30)
-        self.precision_slider.setRange(1, 4)
+        self.precision_slider.setRange(1, 3)
         self.precision_slider.setValue(3)
         self.precision_slider.setFocusPolicy(Qt.NoFocus)
         self.precision_slider.setPageStep(3)
         self.precision_slider.setTickPosition(QSlider.TicksBelow)
-        self.precision_slider.setTickInterval(4)
+        self.precision_slider.setTickInterval(3)
         self.precision_slider.valueChanged[int].connect(self.updatePrecisionValue)
-
+        self.precision_text = QLabel("align() precision: ",self)
         self.precision_number_label = QLabel(f"{self.precision_slider.value()}", self)
         self.precision_number_label.setAlignment(Qt.AlignVCenter)
 
-        self.setWidget(mainWidget)
-        mainWidget.setLayout(QVBoxLayout())
+        self.filename_line = QLineEdit()
+        self.filename_line.setText(default_outfile_name)
 
-        mainWidget.layout().addWidget(pos_button)
-        mainWidget.layout().addWidget(align_button)
-        mainWidget.layout().addWidget(self.precision_slider)
-        mainWidget.layout().addWidget(self.precision_number_label)
-        mainWidget.layout().addWidget(self.filename_line)
+        top_layout = QVBoxLayout()
+        precision_layout = QHBoxLayout()
+        #last_layout = QVBoxLayout()
+
+        top_layout.addWidget(pos_button)
+        top_layout.addWidget(align_button)
+
+        precision_layout.addWidget(self.precision_text)
+        precision_layout.addWidget(self.precision_number_label)
+        precision_layout.addWidget(self.precision_slider)
+        top_layout.addLayout(precision_layout)
+
+        top_layout.addWidget(self.filename_line)
+
+        mainWidget = QWidget(self)
+        mainWidget.setLayout(top_layout)
+        self.setWidget(mainWidget)
 
 
     def updatePrecisionValue(self):
